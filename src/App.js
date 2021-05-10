d82e6abba01a733a9eb798270f7f8504704ae94a
Katia <katia.sfihi@gmail.com>
2021-05-10 18:05:18 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Mules from './component/Mules'
+import './App.scss';
+
+function App() {
+  return (
+    <div className="body-container">
+      <Mules />
+      <Mules />
+      <Mules />
+    </div>
+  );
+}
+
+export default App;
+
---
+++ src/component/Mules.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Mules.scss'
+
+const Mules = () => {
+
+    return (
+    <div className="container">
+        <img src="./sample-image.png" alt="sandals"></img>
+        <h1 className="container__h1">Mules Of The Moment</h1>
+        <p className="container__para">Ideal for warmer weather</p>
+        <span className="container__read-more">Read More</span>
+    </div>
+    )
+}
+
+export default Mules
